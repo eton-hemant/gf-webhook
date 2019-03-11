54b88c31c9ea13efc14f9c5d08e35bd3b3dc2133
eton-hemant <34415215+eton-hemant@users.noreply.github.com>
2019-03-11 11:37:16 +0400

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,16 @@
 <?php
 
-//extract($_POST['body']);
-$_____data = $_POST;
-
 require_once('app/init.php');
+
+$_____data = $_POST;
 
 $emailData = array(
     'fromEmail'=> 'no-reply@etoninstitute.com',
     'fromName'=> 'Eton Institute - Instructor Portal', 
-    'toEmail'=> 'hemantkumar.panchal@etoninstitute.com', 
-    'toName'=> 'hemantkumar Panchal', 
+    'toEmail'=> 'it@etoninstitute.com', 
+    'toName'=> 'IT', 
     'subject'=> 'Test Web Hook - Response', 
-    'message'=> 'Hemant!'.print_r($_____data. true).'<--'
+    'message'=> 'Response!'.print_r($_____data. true).'<--'
 );
 
 sendEmail($emailData);
